344d24dccfcb5fc60ca33d8bb77d185b199b860e
Dominik <d.loessl@milveo.com>
2024-06-17 10:49:52 +0400

    added logos, Skills/Tools, Projects, Testimonials

---
+++ public/images/logos/index.ts
@@ -0,0 +1,73 @@
+import AgileIcon from './agile.png';
+import AmazonLogo from './amazon.png';
+import AsanaLogo from './asana.svg';
+import AWSLogo from './aws.png';
+import ChatGPTLogo from './chatgpt.png';
+import ClickUpLogo from './clickup.png';
+import DHLLogo from './dhl.svg';
+import DockerLogo from './docker.png';
+import DPDLogo from './dpd.png';
+import FedexLogo from './fedex.png';
+import GitLogo from './git.png';
+import GithubActionsIcon from './githubactions.png';
+import GraphQLLogo from './graphql.png';
+import InternetmarkeLogo from './internetmarke.jpg';
+import JiraLogo from './jira.png';
+import JSLogo from './js.png';
+import KanbanLogo from './kanban.jpg';
+import KlaviyoLogo from './klaviyo.png';
+import KubernetesLogo from './kubernetes.png';
+import MapsLogo from './maps.png';
+import MySQLLogo from './mysql.png';
+import NextJSLogo from './nextjs.svg';
+import NodeJSLogo from './nodejs.svg';
+import NoSQLLogo from './nosql.png';
+import PostgresSQLLogo from './postgressql.png';
+import PrompIcon from './prompt.png';
+import ReactLogo from './react.svg';
+import ShopifyLogo from './shopify.png';
+import StorybookLogo from './storybook.png';
+import StripeLogo from './stripe.png';
+import TailwindCSSLogo from './tailwindcss.png';
+import TrelloLogo from './trello.png';
+import TSLogo from './ts.png';
+import VercelLogo from './vercel.png';
+import VSCodeInsidersLogo from './vscodeinsiders.png';
+
+export {
+    AgileIcon,
+    AmazonLogo,
+    AsanaLogo,
+    AWSLogo,
+    ChatGPTLogo,
+    ClickUpLogo,
+    DHLLogo,
+    DockerLogo,
+    DPDLogo,
+    FedexLogo,
+    GitLogo,
+    GithubActionsIcon,
+    GraphQLLogo,
+    InternetmarkeLogo,
+    JiraLogo,
+    JSLogo,
+    KanbanLogo,
+    KlaviyoLogo,
+    KubernetesLogo,
+    MapsLogo,
+    MySQLLogo,
+    NextJSLogo,
+    NodeJSLogo,
+    NoSQLLogo,
+    PostgresSQLLogo,
+    PrompIcon,
+    ReactLogo,
+    ShopifyLogo,
+    StorybookLogo,
+    StripeLogo,
+    TailwindCSSLogo,
+    TrelloLogo,
+    TSLogo,
+    VercelLogo,
+    VSCodeInsidersLogo,
+};
